3e24adb4932dded0398d47f26e119319b77cd433
moonhee.kim <kimmoonh22@gmail.com>
2022-01-23 21:35:31 +0900

    Player 스크립트 추가 (체력)

---
+++ Assets/Scripts/MonoBehaviours/Character.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    public int hitPoints; // 체력
+    public int maxHitPoints; // 체력 최대값
+    
+}
