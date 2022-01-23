62558ffe272dabf1ab0d813c6d819a22869e06af
moonhee.kim <kimmoonh22@gmail.com>
2022-01-24 01:29:26 +0900

    인벤토리 슬롯 및 아이템 획득 동작 스크립트 추가

--- Assets/Scripts/MonoBehaviours/Player.cs
+++ Assets/Scripts/MonoBehaviours/Player.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class Player : Character
 {
+    public Inventory inventoryPrefab;
+    Inventory inventory;
 
     public HealthBar healthBarPrefab;
     HealthBar healthBar;
 
     public void Start()
 	{
+	    inventory = Instantiate(inventoryPrefab);
+	    
         hitPoints.value = startingHitPoints;
         healthBar = Instantiate(healthBarPrefab);
         healthBar.character = this;
@@ -26,7 +30,7 @@ public class Player : Character
                 switch (hitObject.itemType)
                 {
                     case Item.ItemType.COIN:
-                        shouldDisappear = true;
+                        shouldDisappear = inventory.AddItem(hitObject);
                         break;
                     case Item.ItemType.HEALTH:
                         shouldDisappear = AdjustHitPoints(hitObject.quantity);
