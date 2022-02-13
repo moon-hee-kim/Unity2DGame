cd9cad42ec8f0c4b64f4fbabb643e933ad53d6ba
moonhee.kim <kimmoonh22@gmail.com>
2022-02-13 23:28:57 +0900

    Player 스폰 포인트 추가

--- Assets/Scripts/Managers/RPGGameManager.cs
+++ Assets/Scripts/Managers/RPGGameManager.cs
@@ -4,26 +4,37 @@ using UnityEngine;
 
 public class RPGGameManager : MonoBehaviour
 {
+    public SpawnPoint playerSpawnPoint;
     public static RPGGameManager sharedInstance = null;
-    
+
     void Awake()
     {
-    	if (sharedInstance != null && sharedInstance != this)
-    	{
-    		Destroy(gameObject);
-    	}
-    	else
-    	{
-    		sharedInstance = this;
-    	}
+        if (sharedInstance != null && sharedInstance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            sharedInstance = this;
+        }
     }
-    
+
     void Start()
     {
-    	SetupScene();
+        SetupScene();
     }
-    
+
     public void SetupScene()
     {
+        SpawnPlayer();
     }
+
+    public void SpawnPlayer()
+    {
+        if (playerSpawnPoint != null)
+        {
+            GameObject player = playerSpawnPoint.SpawnObject();
+        }
+    }
+
 }
