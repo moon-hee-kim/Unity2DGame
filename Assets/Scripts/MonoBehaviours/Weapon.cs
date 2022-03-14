04b0d228c8d36a673859c0dd4011e83420809c5a
moonhee.kim <kimmoonh22@gmail.com>
2022-03-14 16:27:00 +0900

    새총 발사 위치 계산 메서드 추가

--- Assets/Scripts/MonoBehaviours/Weapon.cs
+++ Assets/Scripts/MonoBehaviours/Weapon.cs
@@ -8,13 +8,14 @@ public class Weapon : MonoBehaviour
 	public GameObject ammoPrefab;
 	static List<GameObject> ammoPool;
 	public int poolSize;
-	public float weaponVelocity;
 	
+	public float weaponVelocity;
 	bool isFiring;
-	[HideInInspector]
-	public Animator animator;
 	
 	Camera localCamera;
+	
+	[HideInInspector]
+	public Animator animator;
 	
 	float positiveSlope;
 	float negativeSlope;
@@ -32,7 +33,49 @@ public class Weapon : MonoBehaviour
 	    animator = GetComponent<Animator>();
 	    isFiring = false;
 	    localCamera = Camera.main;
+	    
+	    Vector2 lowerLeft = localCamera.ScreenToWorldPoint(new Vector2(0, 0));
+	    Vector2 lowerRight = localCamera.ScreenToWorldPoint(new Vector2(Screen.width, 0));
+	    Vector2 upperLeft = localCamera.ScreenToWorldPoint(new Vector2(0, Screen.height));
+	    Vector2 upperRight = localCamera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+	    
+	    positiveSlope = GetSlope(lowerLeft, upperRight);
+	    negativeSlope = GetSlope(upperLeft, lowerRight);
 	}
+	
+	bool HigherThanPositiveSlopeLine()
+	{
+	    Vector2 playerPosition = gameObject.transform.position;
+	    Vector2 mousePosition = localCamera.ScreenToWorldPoint(inputPosition);
+	    
+	    float yIntercept = playerPosition.y - (positiveSlope * playerPosition.x);
+	    float inputIntercept = mousePosition.y - (positiveSlope * mousePosition.x);
+	    
+	    return inputIntercept > yIntercept;
+	}
+	
+	Quadrant GetQuadrant()
+    {
+        bool higherThanPositiveSlopeLine = HigherThanPositiveSlopeLine(Input.mousePosition);
+        bool higherThanNegativeSlopeLine = HigherThanNegativeSlopeLine(Input.mousePosition);
+
+        if (!higherThanPositiveSlopeLine && higherThanNegativeSlopeLine)
+        {
+            return Quadrant.East;
+        }
+        else if (!higherThanPositiveSlopeLine && !higherThanNegativeSlopeLine)
+        {
+            return Quadrant.South;
+        }
+        else if (higherThanPositiveSlopeLine && !higherThanNegativeSlopeLine)
+        {
+            return Quadrant.West;
+        }
+        else
+        {
+            return Quadrant.North;
+        }
+    }
 	
 	void Awake()
 	{
@@ -60,6 +103,11 @@ public class Weapon : MonoBehaviour
 	    UpdateState();
 	}
 	
+	float GetSlope(Vector2 point_a, Vector2 point_b)
+	{
+	    return (point_b.y - point_a.y) / (point_b.x - point_a.x);
+	}
+	
 	GameObject SpawnAmmo(Vector3 location)
 	{
 	    foreach (GameObject ammo in ammoPool)
@@ -74,6 +122,45 @@ public class Weapon : MonoBehaviour
 	    }
 	    
 	    return null;   
+	}
+	
+	void UpdateState()
+	{
+        if (isFiring)
+        {
+            Vector2 quadrantVector;
+            Quadrant quadEnum = GetQuadrant();
+
+            switch (quadEnum)
+            {
+                case Quadrant.East:
+                    quadrantVector = new Vector2(1.0f, 0.0f);
+                    break;
+                case Quadrant.South:
+                    quadrantVector = new Vector2(0.0f, -1.0f);
+                    break;
+                case Quadrant.West:
+                    quadrantVector = new Vector2(-1.0f, 0.0f);
+                    break;
+                case Quadrant.North:
+                    quadrantVector = new Vector2(0.0f, 1.0f);
+                    break;
+                default:
+                    quadrantVector = new Vector2(0.0f, 0.0f);
+                    break;
+            }
+
+            animator.SetBool("isFiring", true);
+            
+            animator.SetFloat("fireXDir", quadrantVector.x);
+            animator.SetFloat("fireYDir", quadrantVector.y);
+
+            isFiring = false;
+        }
+        else
+        {
+            animator.SetBool("isFiring", false);
+        }
 	}
 	
 	void FireAmmo()
